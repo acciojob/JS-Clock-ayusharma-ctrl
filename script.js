2c6d4e3eed2e05e0ad904562822fd2020cc71328
Ayush Sharma <117670536+ayusharma-ctrl@users.noreply.github.com>
2023-02-15 03:54:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-//your code here
-var hourHand = document.querySelector('.hour-hand');
-var minHand = document.querySelector('.min-hand');
-var secondHand = document.querySelector('.second-hand');
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
-function showTime(){
-  const currTime = new Date();
-  const seconds = currTime.getSeconds();
+function setDate() {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
   const secondsDegrees = ((seconds / 60) * 360) + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
-  const minutes = currTime.getMinutes();
+  const minutes = now.getMinutes();
   const minutesDegrees = ((minutes / 60) * 360) + ((seconds/60)*6) + 90;
-  minHand.style.transform = `rotate(${Math.floor(minutesDegrees)}deg)`;
+  minuteHand.style.transform = `rotate(${Math.floor(minutesDegrees)}deg)`;
 
-  const hours = currTime.getHours();
+  const hours = now.getHours();
   const hoursDegrees = ((hours / 12) * 360) + ((minutes/60)*30) + 90;
   hourHand.style.transform = `rotate(${Math.floor(hoursDegrees)}deg)`;
 }
-setInterval(showTime, 1000);
-showTime()
+
+setInterval(setDate, 1000);
+
+setDate();
